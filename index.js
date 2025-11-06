29a89ad9d4f1eb668b0be1795466e776fbc81413
KILLER BOY KING <deepakkanash56@gmail.com>
2025-11-06 16:22:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ try {
 }
 
 // ✅ Group Info
-const GROUP_THREAD_ID = "24196335160017473";
-const LOCKED_GROUP_NAME = "🤪 EXIT FUNNY KIDX + TUSHAR BOKA CHUDKE DAFAN 😂";
+const GROUP_THREAD_ID = "24084619211237811";
+const LOCKED_GROUP_NAME = "🤪 AJAY BHOSDIWALA KALAP GAYA TOO MERI ID KE MSG BLOCKER ID ADD KARKE BLOCK KAR DIYA SUWAR SALA 🤣😂";
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
